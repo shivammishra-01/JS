d1238549a9b315af657f002aedaf55e100ba4774
shivam kumar mishra <111664542+shivamkumar628@users.noreply.github.com>
2024-08-12 20:58:52 +0530

    add variable

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 20130284
+let accountEmail = "shivamholy628@gmail.com"
+var accountPassword = "785501"
+accountcity = "darbhanga"
+
+// accountId =12344    {this not allow to cont value is change}
+
+console.log(accountId);
+
+accountEmail="supeershivamtrick@gmail.com"
+accountPassword ="-2858698"
+accountcity = "bhuwneswar"
+let accountState ;
+/*
+perfer to not use var
+because of issue in block scope and function scope
+
+*/
+console.table([accountId, accountEmail ,accountPassword , accountcity ,accountState]);
