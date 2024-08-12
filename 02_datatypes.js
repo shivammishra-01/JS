50ed15ea41c093ee55ea642ca64b9b52c61fb84f
shivam kumar mishra <111664542+shivamkumar628@users.noreply.github.com>
2024-08-12 23:48:56 +0530

    data

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict "; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs ,not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("shivammishra")
+
+let name ="shivam" //string types
+let age = 21       // interger
+let isLoggedIn = false // Boolean
+
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false 
+//  null => standalone value
+//  undefined =>
+//  symbol => unique
+//  object => 
+
+
+console.log(typeof null)
+console.log(typeof undefined)
+
