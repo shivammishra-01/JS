e19d291e9162fe1aeb8925b0bc1d4c5c6bc59fbe
shivam kumar mishra <111664542+shivamkumar628@users.noreply.github.com>
2024-08-16 00:13:57 +0530

    nums and math

---
+++ 06nums_maths.js
@@ -0,0 +1,15 @@
+const number =500
+
+console.log(number);
+console.log(number.toString().length);//converted into string 
+
+
+console.log(number.toFixed(2));//after the number just like this 100.00
+
+const hundreds= 100000
+console.log(hundreds.toLocaleString(2));//show to coma  easy to read large number
+
+console.log(hundreds.toLocaleString('en-IN'));//show to coma  easy to read large number of indian
+
+
+
