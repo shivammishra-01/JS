c588c3e6aaa6e51cc7f110b00fd4a7c0ec48a69e
shivam kumar mishra <111664542+shivamkumar628@users.noreply.github.com>
2024-08-20 17:29:18 +0530

    change

---
+++ 01_basic/08_Array.js
@@ -0,0 +1,23 @@
+const myArray=[1,2,3,4,5,6,8]
+
+console.log(myArray[1]);
+myArray.push(7)
+console.log(myArray);
+myArray.pop()
+console.log(myArray);
+
+
+// myArray.unshift(9)
+// myArray.shift()
+// console.log(myArray.includes(6))
+// console.log(myArray);
+
+// JOIN
+// const newArr= myArray.join()     //convertd into string 
+// console.log(typeof newArr);
+
+//SLICE AND SPLICE
+
+
+
+
